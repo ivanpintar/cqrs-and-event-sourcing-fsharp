7c8158461d8a99f5b53850acc07953d94247b767
Ivan Pintar <pinetree.rg@gmail.com>
2016-10-02 13:24:54 +0200

    added product creation tests

--- PinetreeShop/PinetreeShop.Domain.Tests/Product/CreateProductTests.cs
+++ PinetreeShop/PinetreeShop.Domain.Tests/Product/CreateProductTests.cs
@@ -1,5 +1,6 @@
 ﻿using PinetreeShop.Domain.Products.Commands;
 using PinetreeShop.Domain.Products.Events;
+using PinetreeShop.Domain.Products.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,15 +13,24 @@ namespace PinetreeShop.Domain.Tests.Product
     public class CreateProductTests : TestBase
     {
         [Fact]
-        [InlineData("One", 1.0)]
-        [InlineData("Two", 2.0)]
-        [InlineData("Three", 3.0)]
-        public void When_ProductCreated_ProductIsCreatedWithTheGivenPrice(string name, decimal price)
+        [InlineData("One", 1)]
+        [InlineData("Two", 2)]
+        [InlineData("Three", 3)]
+        public void When_ProductCreated_ProductIsCreatedWithTheGivenPrice(string name, int price)
         {
             Guid id = Guid.NewGuid();
 
             When(new CreateProduct(id, name, price));
             Then(new ProductCreated(id, name, price));
+        }
+
+        [Fact]
+        public void When_CreatingProductWithSameGuid_ThrowProducExistsException(string name, decimal price)
+        {
+            Guid id = Guid.NewGuid();
+
+            Given(new ProductCreated(id, "One", 1));
+            WhenTrows<ProductExistsException>(new CreateProduct(id, "One", 1));
         }
     }
 }
